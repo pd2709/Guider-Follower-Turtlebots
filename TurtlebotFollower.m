662839065302c6c23aa518ea4f12ca15baaec0b2
12004026 <juan.m.hermosisima@student.uts.edu.au>
2022-05-10 12:29:35 +1000

    added image functionality, images are saved within the TurtlebotFollower object

--- TurtlebotFollower.m
+++ TurtlebotFollower.m
@@ -6,6 +6,13 @@ classdef TurtlebotFollower < handle
         IPAddress           % IP address of the ROS HOST - Saved for possible future use
         velPub              % Velocity Publisher
         velMsg              % Velocity Publisher Message
+        imgSub              % Image Subscriber
+        imgMsg              % Saved rbg image message
+        depthSub            % Depth Subscriber
+        depthMsg            % Saved depth image message
+
+        obsImg              % Saved rgb image, converted from ROS message to imread style image
+        obsDepth            % Saved depth image, converted from ROS message to imread style image
     end
     
     methods
@@ -14,6 +21,8 @@ classdef TurtlebotFollower < handle
         function object = TurtlebotFollower(IPInput)
             rosinit(IPInput,11311)
             [object.velPub, object.velMsg] = rospublisher("/cmd_vel", "geometry_msgs/Twist", "DataFormat", "struct");
+            object.imgSub = rossubscriber("/camera/rgb/image_raw","sensor_msgs/Image","DataFormat","struct");
+            object.depthSub = rossubscriber("camera/depth/image_raw","sensor_msgs/Image","DataFormat","struct");
             object.IPAddress = IPInput;
         end
         
@@ -31,6 +40,49 @@ classdef TurtlebotFollower < handle
         % Publish the command velocity
         function [ ] = pubVelocity(object)
             send(object.velPub,object.velMsg);
+        end
+
+        % Get the rgb image message
+        function rgb = getRGB(object)
+            rgb = object.imgMsg;
+        end
+
+        % Function to update the rgb image
+        function [ ] = updateRGB(object)
+            object.imgMsg = object.imgSub.LatestMessage;
+        end
+
+        % Get the depth image message
+        function depth = getDepth(object)
+            depth = object.depthMsg;
+        end
+
+        % Function to update the depth image
+        function [ ] = updateDepth(object)
+            object.depthMsg = object.depthSub.LatestMessage;
+        end
+
+        % Update usable rgb image
+        function [ ] = updateObsImg(object)
+            object.updateRGB();
+            object.obsImg = rosReadImage(object.imgMsg);
+        end
+
+        % Update usable depth image
+        function [ ] = udpateObsDepth(object)
+            object.updateDepth();
+            object.obsDepth = rosReadImage(object.depthMsg);
+        end
+
+        function [ ] = updateCmdVel(object)
+            img = object.obsImg
+            depth = object.obsDepth
+
+            % VISUAL SERVOING CODE %
+
+            cmdVel = ;
+
+            object.updateCmdVel(cmdVel);
         end
     end
 end
--- main.m
+++ main.m
@@ -1,8 +1,15 @@
 function [] = main(IPAddress)
     Turtle = TurtlebotFollower(IPAddress);
     Turtle.setVelocity(0,0)
-    r = rosrate(10);
-    for i = 1:20
+    r = rosrate(2);
+
+    receive(Turtle.imgSub); % Wait to receive an image message before starting the loop
+    receive(Turtle.depthSub);
+
+    while(1) % for i = 1:20
+        Turtle.updateObsRGB;
+        Turtle.updateObsDepth;
+        Turtle.updateCmdVel;
         Turtle.pubVelocity;
         waitfor(r);
     end
